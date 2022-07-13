409c924a998e1c0f4162be70cad7dc4eb2b84d7c
Charles Jhong <charlesjhongc@gmail.com>
2022-07-13 21:06:12 +0800

    add example contracts

---
+++ contracts/RedToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RedToken is IERC20, ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+}
