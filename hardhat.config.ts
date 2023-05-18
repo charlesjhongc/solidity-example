8aee184e8adedf8604014b6eeef16a8167ed1bc7
Charles Jhong <charlesjhongc@gmail.com>
2023-05-19 00:22:18 +0800

    fix url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,21 +4,22 @@ import "@nomiclabs/hardhat-waffle"
 // This adds support for typescript paths mappings
 import "tsconfig-paths/register"
 
-const ALCHEMY_TOKEN = process.env.ALCHEMY_TOKEN || ""
+const ALCHEMY_ETHEREUM_MAINNET_TOKEN = process.env.ALCHEMY_ETHEREUM_MAINNET_TOKEN || ""
+const ALCHEMY_ETHEREUM_GOERLI_TOKEN = process.env.ALCHEMY_ETHEREUM_GOERLI_TOKEN || ""
 
 module.exports = {
     networks: {
         hardhat: {
             forking: {
-                url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_TOKEN}`,
+                url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ETHEREUM_MAINNET_TOKEN}`,
                 blockNumber: 17287570,
             },
         },
         mainnet: {
-            url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_TOKEN}`,
+            url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ETHEREUM_MAINNET_TOKEN}`,
         },
         goerli: {
-            url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_TOKEN}`,
+            url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_ETHEREUM_GOERLI_TOKEN}`,
         },
     },
     solidity: {
